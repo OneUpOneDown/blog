01b225fcbc9d0fab721b5059b82d8c1fb71f62c2
ljz <447396752@qq.com>
2016-07-20 17:58:24 +0800

    样式修改

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(flash());
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 var mongodb = require('mongodb').MongoClient;
 var settings = require('../settings');
+var markdown = require('markdown').markdown;
        function Post(name, title, post) {
          this.name = name;
          this.title = title;
@@ -43,7 +44,7 @@ var settings = require('../settings');
            });
          });
        };
-       Post.get = function(name, callback) {
+       Post.getAll = function(name, callback) {
          mongodb.connect(settings.url,function (err, db) {
            if (err) {
              return callback(err);
@@ -69,3 +70,33 @@ var settings = require('../settings');
            });
          });
        };
+       //获取一篇文章
+Post.getOne = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.connect(settings.url,function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        db.close();
+        return callback(err);
+      }
+      //根据用户名、发表日期及文章名进行查询
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function (err, doc) {
+        db.close();
+        if (err) {
+          return callback(err);
+        }
+        //解析 markdown 为 html
+        doc.post = markdown.toHTML(doc.post);
+        callback(null, doc);//返回查询的一篇文章
+      });
+    });
+  });
+};
